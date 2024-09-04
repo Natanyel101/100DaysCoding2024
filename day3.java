13ecc331f6e8fcaa0e3653f96840891c33dc65de
Natanyel101 <nielneil6625@gmail.com>
2024-09-04 08:37:50 +0800

    Day 3

--- day3.java
+++ day3.java
@@ -1,13 +1,10 @@
 public class day2{
     public static void main(String[] args) {
+   
      
-     String nama = "Natanyel";
-     char oke ='A';
-     
-     
-     System.out.println("NAMA : " + nama);
+     System.out.println("NAMA : Natanyel A.G " );
      System.out.println("NIM :D0224302");
-     System.out.println("kelas : " + oke);
+     System.out.println("kelas : INFORMATIKA A");
     }
     
 }
