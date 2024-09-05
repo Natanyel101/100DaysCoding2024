496fce61c0c6159c4f5da7059932ed76e40d6ced
Natanyel101 <nielneil6625@gmail.com>
2024-09-05 13:09:24 +0800

    Day 4

---
+++ day4.java
@@ -0,0 +1,12 @@
+public class day4{
+    public static void main(String[] args) {
+     
+     String nama = "D0224302";
+     char oke ='A';
+     
+     System.out.print("NAMA: natanyel\n");
+     System.out.printf("NIM : %s \n", nama);
+     System.out.printf("kelas : %c \n", oke);
+    }
+}
+ 
