1642a0c01518f4e9ed4e2d81deb99c083e9ad63c
Natanyel101 <nielneil6625@gmail.com>
2024-09-10 08:05:02 +0800

    Day 9

---
+++ day9.java
@@ -0,0 +1,16 @@
+public class day9{
+    
+    public static void main(String[] args) {
+        //print
+        System.out.print("NAMA: Natanyel\n");
+        //variable final (constanta)
+       final String nim= "D0224302";
+        //print
+        System.out.println("NIM: "+ nim);
+        //variable final (constanta)
+       final char kelas='A';
+        //print
+        System.out.printf("KELAS: %c \n",kelas);
+    }
+    
+}
