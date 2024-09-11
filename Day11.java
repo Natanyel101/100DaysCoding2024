73ccebb7f735114982222b770cd7926c95b2b5ae
Natanyel101 <nielneil6625@gmail.com>
2024-09-12 07:33:26 +0800

    Day 11

---
+++ Day11.java
@@ -0,0 +1,11 @@
+
+public class day11 {
+    
+    public static void main(String[] args) {
+        /*short hampir mirip dengan byte,short mencakup
+         dari -32.768 sampai 32.767 */
+        short no = 32400;
+        System.out.println(no);
+    }
+    
+}
