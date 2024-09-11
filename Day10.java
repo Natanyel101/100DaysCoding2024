4958717d0643e65a9089decf8b4598562a03871d
Natanyel101 <nielneil6625@gmail.com>
2024-09-11 15:07:06 +0800

    Day 10

---
+++ Day10.java
@@ -0,0 +1,9 @@
+public class day10{
+    
+    public static void main(String[] args) {
+        //byte hanya mencakup angka -128 sampai 127
+        byte no =48;
+        System.out.println(no);
+    }
+    
+}
