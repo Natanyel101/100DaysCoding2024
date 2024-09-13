3a676c1b3dbc6e736864e1bb5428ddf305fac708
Natanyel101 <nielneil6625@gmail.com>
2024-09-13 13:04:51 +0800

    Day 12 int/integer

---
+++ Day12.java
@@ -0,0 +1,10 @@
+
+public class day12 {
+    
+    public static void main(String[] args) {
+        //int mencakup hanya 9 digit angka
+        int no = 123456789;
+        System.out.println(no);
+    }
+    
+}
