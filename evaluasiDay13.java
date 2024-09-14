cb495fd20f15b91f860db5fbfe09f05914c17964
Natanyel101 <nielneil6625@gmail.com>
2024-09-14 21:19:29 +0800

    Evaluasi mentor day 13

---
+++ evaluasiDay13.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class evaluasi {
+    
+    public static void main(String[] args) {
+        //variable
+        String nama="Nataniel";
+        int umur =17;
+        //print
+        System.out.printf("nama: %s \n", nama);
+        System.out.println("umur: "+ umur);
+        System.out.print("alamat: mamasa");
+    }
+    
+}
