d9a3e25b34d76100d8113e4030a30a7dd0230f10
Natanyel101 <nielneil6625@gmail.com>
2024-09-15 22:40:11 +0800

    Day 14

---
+++ Day14.java
@@ -0,0 +1,11 @@
+
+public class day14 {
+    
+    public static void main(String[] args) {
+        //long mencakup 9,2 kuadriliun angka
+        //dan di akhiri dengan (L),apabila tidak,maka akan di baca sebagai (int)
+        long no=1111111111111111111L;
+        System.out.println(no);
+    }
+    
+}
