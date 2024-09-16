67db7746d1be558e9d24117f6d1d8cc3992b1e46
Natanyel101 <nielneil6625@gmail.com>
2024-09-16 17:43:16 +0800

    Day 15 double

---
+++ Day15.java
@@ -0,0 +1,10 @@
+
+public class day15 {
+    
+    public static void main(String[] args) {
+        //double untuk angka desimal
+        double no=12.100001;
+        System.out.println(no);
+    }
+    
+}
