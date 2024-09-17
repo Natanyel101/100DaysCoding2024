10e74af67c2d54399091eb09ffaa0084843e5bb7
Natanyel101 <nielneil6625@gmail.com>
2024-09-17 18:23:34 +0800

    Day 16 float

---
+++ Day16.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class day16 {
+    
+    public static void main(String[] args) {
+        //float untuk angka desimal namun harus ad (f) di belakang angka
+        float no=12.33f;
+        System.out.println(no);
+    }
+    
+}
