9d6f66f4d8465ab8e2f8cfc6361fe29801449059
Natanyel101 <nielneil6625@gmail.com>
2024-09-18 20:05:50 +0800

    Day 17  char

---
+++ Day17.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class day17 {
+    
+    public static void main(String[] args) {
+        //char untuk satu huruf saja 
+        char inisial = 'N';
+        String nama ="NATANYEL";
+        //print
+        System.out.println("nama: " + nama);
+        System.out.printf("inisial %c \n", inisial);
+    }
+    
+}
