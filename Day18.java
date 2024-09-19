52b72a6def061b2246c195febb364a953f493ad7
Natanyel101 <nielneil6625@gmail.com>
2024-09-19 19:37:34 +0800

    Day 18 boolean

---
+++ Day18.java
@@ -0,0 +1,17 @@
+package isc;
+
+public class day18 {
+    
+    public static void main(String[] args) {
+        //boolean hanya punya dua value yaitu (true dan false)
+        String z= "ayam";
+        String e= "kerbau";
+        boolean b= z==e;//boolean untuk mengetahui yang mana petnyataan kita benar atau tidak
+        
+        //print
+        System.out.println(z);
+        System.out.println(e);
+        System.out.println(b);
+    }
+    
+}
