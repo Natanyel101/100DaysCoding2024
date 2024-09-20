93314b84c2ddc8e94679c80321194f6467fab44f
Natanyel101 <nielneil6625@gmail.com>
2024-09-20 20:34:06 +0800

    Day 19 String

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class day19 {
+    
+    public static void main(String[] args) {
+        //string mencakup kata atau kalimat maupun huruf
+        String nama="Natanyel";
+        String aname="Niel";
+        System.out.println("nama: " + nama);
+        System.out.println("sering di panggil: "+ aname);
+    }
+    
+}
