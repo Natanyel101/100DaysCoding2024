7e701fa36ae048f69eb2f8930fdad37decb9b84d
Natanyel101 <nielneil6625@gmail.com>
2024-09-22 20:19:00 +0800

    Day 21

---
+++ Day21.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class day21 {
+    
+    public static void main(String[] args) {
+        // tipe data short
+        Short no = 12233;//short mencakup angka dari -32768 sampai 32767
+        Short ok = 32767;
+        //print
+        System.out.println("hasil: "+12223);
+        System.out.println("1223 + 32767 hasil: "+(no+ok));
+     }
+    
+}
