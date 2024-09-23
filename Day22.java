72ad1eb74e0c696f104a85adb693bd28d484ea03
Natanyel101 <nielneil6625@gmail.com>
2024-09-23 22:37:33 +0800

    Day 22

---
+++ Day22.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class day22 {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan nilai 233
+        Integer no = 233;
+
+        //  pembagian dengan 12 dan simpan hasil
+        int result = no / 12;
+
+        // print hasil pembagian
+        System.out.println("Hasil pembagian dari " + no + " dibagi 12 adalah: " + result);
+    }
+}
