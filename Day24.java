75f7348142a0ab7db1662d142c64560d87451534
Natanyel101 <nielneil6625@gmail.com>
2024-09-25 20:59:27 +0800

    Day24

---
+++ Day24.java
@@ -0,0 +1,20 @@
+package isc;
+
+public class day24 {
+    
+    public static void main(String[] args) {
+        //float
+        Float no= 4.0F;
+        //printf
+        System.out.printf("hasil tambah: %f \n", (no+3));
+        
+        System.out.printf("hasil kali: %f \n",(no*3));
+        
+        System.out.printf("hasil kurang: %f \n",(no-3));
+        
+        System.out.printf("hasil bagi: %f \n",(no/3));
+        
+        System.out.printf("hasil sisa bagi: %f \n",(no%3));
+    }
+    
+}
