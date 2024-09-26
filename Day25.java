fab502ee1871a559c39651d7fe395639bb45dd3a
Natanyel101 <nielneil6625@gmail.com>
2024-09-26 20:49:40 +0800

    Day 25

---
+++ Day25.java
@@ -0,0 +1,19 @@
+package isc;
+
+public class day25 {
+    
+    public static void main(String[] args) {
+        System.out.println("OPERATOR ARITMATIKA");
+        Double no=3.0;
+        System.out.println("angka pertama: "+no);
+        Double ok=4.0;
+        System.out.println("angka kedua: "+ok);
+        System.out.println("|•H•A•S•I•L•|");
+        System.out.printf("hasil tambah: %f \n",(no+ok));
+        System.out.printf("hasil kurang: %f \n",(no-ok));
+        System.out.printf("hasil kali: %f \n", (no*ok));
+        System.out.printf("hasil bagi %f \n", (no/ok));
+        System.out.printf("hasil sisa bagi: %f \n",(no%ok));
+    }
+    
+}
