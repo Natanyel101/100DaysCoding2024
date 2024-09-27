9b1d0f759a94c025be73a1265830c06daf3d409e
Natanyel101 <nielneil6625@gmail.com>
2024-09-27 21:26:03 +0800

    Day 26

---
+++ Day26.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class day26 {
+    
+    public static void main(String[] args) {
+        Character inisial='N';
+        String nama ="Natanyel";
+        System.out.println("inisial:"+inisial);
+        System.out.println("nama: "+nama);
+    }
+    
+}
