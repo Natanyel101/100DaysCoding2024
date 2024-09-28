1dedd8bc457fbcec271c026baf41c36b7d9ba6a2
Natanyel101 <nielneil6625@gmail.com>
2024-09-28 19:58:52 +0800

    Day27no1

---
+++ Day27no1.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class day27no1 {
+    
+    public static void main(String[] args) {
+        System.out.println("selamat datang di perpustakaan film favorit");
+        final int jumlahFilm=50;
+        String namaFilm="iron man";
+        namaFilm ="from";
+        Short tahunRilis=2010;
+        System.out.printf("film berjudul %s di rilis pada tahun %d dan terdapat %d film dalam kategori saat ini",namaFilm,tahunRilis,jumlahFilm);
+    }
+    
+}
