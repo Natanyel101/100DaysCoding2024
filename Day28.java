6bee13ee9025f23ac39e67a108ac946d934b3c49
Natanyel101 <nielneil6625@gmail.com>
2024-09-29 22:23:23 +0800

    Day 28

---
+++ Day28.java
@@ -0,0 +1,16 @@
+package isc;
+
+public class day28 {
+    
+    public static void main(String[] args) {
+        int a=12;//variable int
+        System.out.println("angka pertama: "+a);//print
+        int b =12;//variable int
+        System.out.println("angka kedua: "+ b);//print
+        Boolean c=a==b;//variable boolean
+        Boolean d=a!=b;//variable boolean
+        System.out.println("apakah angka pertama dan kedua itu sama? \njawab: "+c);//print
+        System.out.println("apakah angka pertama dan kedua itu tidak sama? \njawab; "+d);//print
+    }
+    
+}
