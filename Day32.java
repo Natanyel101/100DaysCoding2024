5b4369549105a76ff373cf6afdaddf6aff0f14fe
Natanyel101 <nielneil6625@gmail.com>
2024-10-03 17:13:29 +0800

    Day 32

---
+++ Day32.java
@@ -0,0 +1,17 @@
+package isc;
+
+public class day32 {
+    
+    public static void main(String[] args) {
+        //variable
+        int a =48;
+        byte b=123;
+        
+        String c= String.valueOf(a);//int ke string
+        String d= String.valueOf(b);//byte ke string
+        //print untung menampilkan
+        System.out.println("hasil int ke string: "+c);
+        System.out.println("hasil byte ke string: "+d);
+    }
+    
+}
