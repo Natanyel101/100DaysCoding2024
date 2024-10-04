39573e397a5288aa77c54e4f27a60801a029849c
Natanyel101 <nielneil6625@gmail.com>
2024-10-04 21:01:09 +0800

    Day 33 casting

---
+++ Day33.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class day33 {
+    
+    public static void main(String[] args) {
+        int abc = 48;  // Tipe data int
+        double zee = abc;  // Otomatis casting dari int ke double
+
+        System.out.println("Nilai int: " + abc);  // Output: 48
+        System.out.println("Nilai double (setelah casting): " + zee);  // Output: 48.0
+    }
+    
+}
