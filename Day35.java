3d923049c1fad5de26ae3fa20e1210dab5489f58
Natanyel101 <nielneil6625@gmail.com>
2024-10-06 21:00:30 +0800

    Day 35

---
+++ Day35.java
@@ -0,0 +1,15 @@
+package isc;
+import java.util.*;
+public class day35 {
+    
+    public static void main(String[] args) {
+        Scanner zee= new Scanner (System.in);
+        System.out.print("masukkan angka pertama: ");
+        int ok =zee.nextInt();
+        System.out.print("masukkan angka kedua: ");
+        int sip=zee.nextInt();
+        System.out.println("hasil penjumlahan: "+(ok+sip));
+        System.out.println("hasil pengurangan: "+(ok-sip));
+    }
+    
+}
