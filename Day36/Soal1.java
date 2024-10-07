d10faf403774af1ee4f4821ebc95cdc017c119d6
Natanyel101 <nielneil6625@gmail.com>
2024-10-07 18:06:59 +0800

    Soal 1

---
+++ Day36/Soal1.java
@@ -0,0 +1,14 @@
+package isc;
+import java.util.*;
+public class soal1 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan jumlah kebun: ");
+        int zee=oke.nextInt();
+        int sip=50;
+        System.out.println("jika petani memiliki "+(zee)+" kebun,maka");
+        System.out.println("penghasilan petani: "+(zee*sip)+"Kg buah");
+    }
+    
+}
