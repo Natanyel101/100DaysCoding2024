4a9e3a136249770a0bd11c24c4c1767c4f5072cc
Natanyel101 <nielneil6625@gmail.com>
2024-10-08 17:41:14 +0800

    Soal 1

---
+++ Day37/soal1.java
@@ -0,0 +1,14 @@
+package isc;
+import java.util.*;
+public class soal1 {
+    
+    public static void main(String[] args) {
+        //imputan
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan jumlah pensil yang akan di beli: ");//print
+        int zee=oke.nextInt();
+        //print
+        System.out.println("awalnya siswa memiliki 0 pensil,\nsetelah ia membeli pensil sebanyak:"+zee+"\nmaka pensil yang di milikinya sekarang adalah:"+zee);
+    }
+    
+}
