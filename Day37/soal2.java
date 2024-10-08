13a43da6239feefba3469e59a05baadded2c353e
Natanyel101 <nielneil6625@gmail.com>
2024-10-08 21:16:10 +0800

    Soal 2

--- Day37/soal2.java
+++ Day37/soal2.java
@@ -8,7 +8,7 @@ public class soal2 {
         int zee=oke.nextInt();
         System.out.print("jumlah penambahan pohon setiap bulan: ");
         int sip=oke.nextInt();
-        System.out.println("awalnya ada:"+zee+"pohon\nsetiap bulan tukang kebun selalu menambah "+sip+"pohon baru,\nmaka setelah satu bulan,tukang kebun memiliki"+(zee+sip)+"pohon");
+        System.out.println("awalnya ada:"+zee+"pohon\nsetiap bulan tukang kebun selalu menambah "+sip+"pohon baru,\nmaka setelah satu bulan,tukang kebun memiliki"+(zee+=sip)+"pohon");
     }
     
 }
