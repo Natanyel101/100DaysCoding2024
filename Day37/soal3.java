7363c8396f14d0af348735db2c9f06fc7b6615eb
Natanyel101 <nielneil6625@gmail.com>
2024-10-08 17:43:21 +0800

    Soal 3

---
+++ Day37/soal3.java
@@ -0,0 +1,14 @@
+package isc;
+import java.util.*;
+public class soal3 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in); 
+        System.out.print("masukkan jumlah barang: ");
+        int zee= oke.nextInt();
+        System.out.print("jumlah barang yang di jual; ");
+        int sip =oke.nextInt();
+        System.out.println("sebuah toko memiliki "+zee+" barang\nsetiap minggu mereka menjual "+sip+" barang\nsisa barang di gudang setelah satu minggu adalah:"+(zee-sip));
+    }
+    
+}
