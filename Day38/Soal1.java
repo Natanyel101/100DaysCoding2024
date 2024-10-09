62429fe856d89338b0937332b938ba8e00ea6b3a
Natanyel101 <nielneil6625@gmail.com>
2024-10-09 16:22:00 +0800

    Soal 1

---
+++ Day38/Soal1.java
@@ -0,0 +1,13 @@
+package isc;
+import java.util.*;
+public class soal1 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan jumlah awal lahan yg di tanami padi: ");
+        int zee=oke.nextInt();
+        zee *=2;
+        System.out.println("luas lahan padi setelah satu tahun adalah:"+zee);
+    }
+    
+}
