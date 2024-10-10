bb903a06e51981925f875c5fce1c48e46995b6a0
Natanyel101 <nielneil6625@gmail.com>
2024-10-10 14:14:36 +0800

    Soal 1

---
+++ Day39/soal1.java
@@ -0,0 +1,14 @@
+package isc;
+import java.util.*;
+public class soal1 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("jumlah apel: ");
+        int zee=oke.nextInt();
+        System.out.print("jumlah monyet: ");
+        int sip=oke.nextInt();
+        System.out.println("sisa apel setelah di bagi kepada:"+sip+" monyet\nadalah: "+(zee%=sip));
+    }
+    
+}
