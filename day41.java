4a513f0b63f7f79d78ffb969525e0ae300defa68
Natanyel101 <nielneil6625@gmail.com>
2024-10-13 17:45:57 +0800

    Day 41 evaluasi

---
+++ day41.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class imputan {
+    
+    public static void main(String[] args) {
+        Scanner zee=new Scanner(System.in);
+        int a=zee.nextInt();
+        int b=zee.nextInt();
+        System.out.println("hasil tambah: "+(a+b));
+        System.out.println("hasil kutang: "+(a-b));
+        System.out.println("hasil kali: "+(a*b));
+        System.out.println("hasil bagi: "+(a/b));
+        System.out.println("hasil sisah bagi: "+(a%b));
+    }
+    
+}
