6d592310d07f158023f654fc22fee97b913576c6
Natanyel101 <nielneil6625@gmail.com>
2024-10-18 20:54:30 +0800

    Day 67 percabangan if

---
+++ day47.java
@@ -0,0 +1,19 @@
+package isc;
+import java.util.*;
+public class day47 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan password: ");
+        String a=oke.nextLine();
+        
+        if (a.equalsIgnoreCase("natanyel")){
+            System.out.println("nama: NATANYEL");
+            System.out.println("umur: 17thn");
+            System.out.println("status: mahasiswa");
+            System.out.println("hoby: ngidol JKT48");
+            System.out.println("oshi: marshajkt48");
+        }
+    }
+    
+}
