99bcc863b26ec78d0b6e85cbc4af2e32f57e6593
Natanyel101 <nielneil6625@gmail.com>
2024-10-20 22:34:09 +0800

    Day 49

---
+++ day49.java
@@ -0,0 +1,26 @@
+package isc;
+import java.util.*;
+public class day49 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan password: ");
+        String a=oke.nextLine();
+        
+        if (a.equalsIgnoreCase("natanyel")){
+            System.out.println("nama: NATANYEL");
+            System.out.println("umur: 17thn");
+            System.out.println("status: mahasiswa");
+            System.out.println("hoby: ngidol JKT48");
+            System.out.println("oshi: marshajkt48");
+         }
+        else if(a.equals("NIEL")){
+            System.out.println("welcome");
+            System.out.println("my name is NIEL");
+            System.out.println("my hoby is coding");
+        }
+        else{
+            System.out.println(" nama tidak sesuai..!!\n silahkan coba lagi");
+        }
+    } 
+}
