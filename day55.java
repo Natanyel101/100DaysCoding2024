ec4bf5031e851dd38b2bfd23323e0594e06d9aa9
Natanyel101 <nielneil6625@gmail.com>
2024-10-26 20:33:32 +0800

    Day 55

---
+++ day55.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class day55 {
+    
+    public static void main(String[] args) {
+        for (int a=10; a>=0; a--)
+        System.out.println("hasil: "+a);
+    }
+    
+}
