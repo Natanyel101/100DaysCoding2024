2c8175d40dc6ac94dae13762684eac0196f3911b
Natanyel101 <nielneil6625@gmail.com>
2024-10-28 21:03:54 +0800

    Day 57

---
+++ day57.java
@@ -0,0 +1,15 @@
+package isc;
+import java.util.*;
+public class day57 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner (System.in);
+        System.out.print("masukkan angka: ");
+        int zee =oke.nextInt();
+        while (zee<=50){
+            System.out.println("angka ke: "+zee);
+            zee++;
+        }
+    }
+    
+}
