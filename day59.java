6a8d271656717553ab2886784523925e92082457
Natanyel101 <nielneil6625@gmail.com>
2024-10-30 21:18:46 +0800

    Day 59

---
+++ day59.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class day59 {
+    
+    public static void main(String[] args) {
+        int[] oke= {4,8,4,8,4,8,48,9};
+        for (int zee : oke) {
+            System.out.println("Angka: " + zee);
+        }
+      }
+    }
+
