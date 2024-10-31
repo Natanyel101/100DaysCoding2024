7a796b15e7b8abd3768c36b46f4f58e2de7cfa3d
Natanyel101 <nielneil6625@gmail.com>
2024-10-31 17:12:02 +0800

    Day 60

---
+++ day60.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class day60 {
+    
+    public static void main(String[] args) {
+        int oke=1;
+        do{
+            System.out.println("hasil: "+oke);
+            oke++;
+        }
+        while(oke<=20);
+    }
+    
+}
