a771d7111d258d72c130206581cd07d54d0b7cdc
Natanyel101 <nielneil6625@gmail.com>
2024-11-03 08:31:39 +0800

    Day 63

--- day63.java
+++ day63.java
@@ -1,14 +1,15 @@
 package isc;
 
-public class day62 {
+public class day63 {
     
     public static void main(String[] args) {
         int a=0;
         for ( ;a<=10; a++){
-            System.out.println("angka ke-"+a);
             if (a==5){
-                break;                
-            }
+                System.out.println("melompat di sini");
+                continue;
+          }
+            System.out.println("angka ke-"+a);
         }
         System.out.println("berhenti di angka ke-"+a);
     }
