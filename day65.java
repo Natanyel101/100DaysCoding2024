49f5a39c7e71aae50481fde310c45206f33c2363
Natanyel101 <nielneil6625@gmail.com>
2024-11-05 22:50:21 +0800

    Day 65

---
+++ day65.java
@@ -0,0 +1,10 @@
+public class day65 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 5; a++) {          
+            for (int b = 1; b <= 2; b++) {       
+                System.out.println("angka ke- " + a + ", angka b ke-" + b);
+            }
+            System.out.println("Selesai \n");
+        }
+    }
+}
