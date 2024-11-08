7de3dc36c10aaccc9e0da5e8048ca68faf357bc8
Natanyel101 <nielneil6625@gmail.com>
2024-11-09 00:09:54 +0800

    Day 68

---
+++ day68.java
@@ -0,0 +1,12 @@
+public class day68 {
+    
+    public int zee(int angka1, int angka2) {
+        int jumlah = angka1 + angka2;
+        return jumlah;  
+    }
+    public static void main(String[] args) {
+        day68 oke = new day68();
+        int sip = oke.zee(7, 3);
+        System.out.println("Hasil Penjumlahan: " + sip);
+    }
+}
