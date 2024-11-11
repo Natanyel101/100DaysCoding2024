105451f8a12b6820e82c9efbb05dd7de140eaa9d
Natanyel101 <nielneil6625@gmail.com>
2024-11-11 22:20:12 +0800

    Day 71

---
+++ day71.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class day71 {
+    public static void main(String[] args) {
+        System.out.print("ini adalah angka-");
+        System.out.println(angka(5));
+    }
+    public static int angka(int zee){
+        return zee;
+    }
+}
