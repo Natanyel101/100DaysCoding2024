528a0e4cf19207b98bc58193020d33a4597f61eb
Natanyel101 <nielneil6625@gmail.com>
2024-11-20 20:27:21 +0800

    Day 80

---
+++ day80.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class day80 {
+    
+    public static void main(String[] args) {
+        String zee=" Natanyel ";
+        String oke=zee.trim();
+        
+        System.out.println("|"+zee+"|");
+        System.out.println("|"+oke+"|");
+    }
+    
+}
