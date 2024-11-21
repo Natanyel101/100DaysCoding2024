f9d801bb6ac61440e54ecc499bbd99942a71aead
Natanyel101 <nielneil6625@gmail.com>
2024-11-21 21:19:21 +0800

    Day 81

---
+++ day81.java
@@ -0,0 +1,13 @@
+package isc;
+import java.util.*;
+public class day81 {
+    
+    public static void main(String[] args) {
+        Scanner oke=new Scanner(System.in);
+        System.out.print("masukkan angka $ ");
+        int zee=oke.nextInt();
+        String ze=String.valueOf(zee);
+        System.out.println("int ke string-"+ze);
+    }
+    
+}
