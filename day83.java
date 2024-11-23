2a43275e8260cd127f69e26e115965daedee539f
Natanyel101 <nielneil6625@gmail.com>
2024-11-23 23:13:14 +0800

    Day 83

---
+++ day83.java
@@ -0,0 +1,11 @@
+package isc;
+public class day83 {
+    public static void main(String[] args) {
+        String[] nama = {"awal", "krieldi", "ari'",};
+        
+        System.out.println("Daftar nama teman:");
+        for (int zee= 0; zee < nama.length; zee++) {
+            System.out.println("nama teman : " + nama[zee]);
+        }
+    }
+}
