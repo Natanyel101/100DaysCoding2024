a35565035b8a12694236bda302fe5aec14a5d8b8
Natanyel101 <nielneil6625@gmail.com>
2024-11-25 20:49:26 +0800

    Day 85

---
+++ day85.java
@@ -0,0 +1,11 @@
+package isc;
+import java.util.*;
+
+public class day85 {
+
+    public static void main(String[] args){
+        int[] zee = {48,19,12,34};
+        
+        System.out.println("hasil: " + Arrays.toString(zee));
+    }
+}
