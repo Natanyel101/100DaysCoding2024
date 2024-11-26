b0f5087c594165e239c8ba13a7a3f51ed1c844d4
Natanyel101 <nielneil6625@gmail.com>
2024-11-26 21:23:26 +0800

    Day 86

---
+++ day86.java
@@ -0,0 +1,10 @@
+package isc;
+public class day86 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        
+        for (int elemen : array) {
+            System.out.println(elemen);
+        }
+    }
+}
