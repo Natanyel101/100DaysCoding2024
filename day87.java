5fd99b7fa4f56ffbc9a4376cd5ba6561754266c5
Natanyel101 <nielneil6625@gmail.com>
2024-11-28 07:03:23 +0800

    Day 87

---
+++ day87.java
@@ -0,0 +1,10 @@
+package isc;
+import java.util.Arrays;
+
+public class day87 {
+    public static void main(String[] args) {
+        int[] zee = {1, 2, 3, 4, 5};
+        zee[3] = 10;
+        System.out.println(Arrays.toString(zee));
+    }
+}
