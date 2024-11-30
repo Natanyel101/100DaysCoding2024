69b719243430b42efa03ad66173aecf9c7e4a0eb
Natanyel101 <nielneil6625@gmail.com>
2024-11-30 21:14:53 +0800

    Day 90

---
+++ day90/soal3.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class soal3 {
+    
+    public static void main(String[] args) {
+        for (int a=50; a>=1; a-=5){
+            System.out.print(" "+a);
+        }
+        
+    }
+    
+}
